ec1584f668f22ed37524216e43048b90e796a607
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2025-02-16 21:11:28 +1000

    pages setup

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Session from "./pages/Session";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Vite + React</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard/:id" element={<Dashboard />} />
+        <Route path="/session/:id" element={<Session />} />
+      </Routes>
+    </Router>
   );
 }
 
